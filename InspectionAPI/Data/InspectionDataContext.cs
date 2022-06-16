7e37f8437045cc9d922200c4b0db9521a20af9e7
jay <jay@PSSPLAHM-JAYP>
2022-06-16 17:16:53 +0530

    Add project files.

---
+++ InspectionAPI/Data/InspectionDataContext.cs
@@ -0,0 +1,16 @@
+﻿using InspectionModel.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace InspectionAPI.Data
+{
+    public class InspectionDataContext : DbContext
+    {
+        public InspectionDataContext(DbContextOptions<InspectionDataContext> options) : base(options) { }
+
+        public DbSet<Inspection> Inspections { get; set; }
+
+        public DbSet<InspectionType> InspectionTypes { get; set; }
+
+        public DbSet<Status> Statuses { get; set; }
+    }
+}
